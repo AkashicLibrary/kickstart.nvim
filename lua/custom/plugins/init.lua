44cadfc6be669783bc24180b2642093a99cac10f
AkashicLibrary <akasha.akashiro@gmail.com>
2025-09-19 10:26:40 +0200

    update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -133,16 +133,16 @@ return {
       end, { desc = 'Kill Session' })
     end,
   },
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-      'MunifTanjim/nui.nvim',
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-  },
+  -- {
+  --   'nvim-neo-tree/neo-tree.nvim',
+  --   branch = 'v3.x',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+  --     'MunifTanjim/nui.nvim',
+  --     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  --   },
+  -- },
   'turbio/bracey.vim',
   'alec-gibson/nvim-tetris',
   'seandewar/nvimesweeper',
